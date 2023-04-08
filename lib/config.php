3e7af74f64c8b5d1b5973cf900a2fb36dd453806
Harsh Mistry <123105167+Harshm43@users.noreply.github.com>
2023-04-07 20:08:24 -0400

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -5,26 +5,26 @@ $ini = @parse_ini_file(".env");
 if($ini && isset($ini["DB_URL"])){
     //load local .env file
     $url = $ini["DB_URL"];
-    $db_url = parse_url($url);
+    $DB_url = parse_url($url);
 }
 else{
     //load from heroku env variables
     $url = getenv("DB_URL");
-    $db_url = parse_url($url);
+    $DB_url = parse_url($url);
     
 }
 //attempts to handle a failure where parse_url doesn't parse properly (usually happens when special characters are included)
-if (!$db_url || count($db_url) === 0) {
+if (!$DB_url || count($DB_url) === 0) {
     $matches;
     $pattern = "/mysql:\/\/(\w+):(\w+)@([^:]+):(\d+)\/(\w+)/i";
     preg_match($pattern, $url, $matches);
-    $db_url["host"] = $matches[3];
-    $db_url["user"] = $matches[1];
-    $db_url["pass"] = $matches[2];
-    $db_url["path"] = "/" . $matches[5];
+    $DB_url["host"] = $matches[3];
+    $DB_url["user"] = $matches[1];
+    $DB_url["pass"] = $matches[2];
+    $DB_url["path"] = "/" . $matches[5];
 }
-$dbhost   = $db_url["host"];
-$dbuser = $db_url["user"];
-$dbpass = $db_url["pass"];
-$dbdatabase       = substr($db_url["path"],1);
+$dbhost   = $DB_url["host"];
+$dbuser = $DB_url["user"];
+$dbpass = $DB_url["pass"];
+$dbdatabase       = substr($DB_url["path"],1);
 ?>
