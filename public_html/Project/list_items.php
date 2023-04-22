235ece65ba0f094d1ed6aa45f4779a4661aad025
Harsh Mistry <123105167+Harshm43@users.noreply.github.com>
2023-04-22 16:34:11 -0400

    Update list_items.php

--- public_html/Project/list_items.php
+++ public_html/Project/list_items.php
@@ -5,7 +5,7 @@ if (!has_role("Admin") && !has_role("Shop Owner")) {
     die(header("Location: $BASE_PATH" . "/home.php"));
 }
 ?>
-<h1 class="left-margin">All Products</h1>
+<h1>Home</h1>
 <?php
 
 if (is_logged_in(false)) {
@@ -15,112 +15,125 @@ if (is_logged_in(false)) {
 //default variables needed for conditional html below
 $category = 'all';
 $search = '';
-$sort = 'none';
+$min = '';
+$max = '';
 $results = [];
-$db = getDB();
-$query = "SELECT name, unit_price, visibility FROM Products";
-$params = [];
-//sd96 - 7/17/22
-if (isset($_POST["search"])) {
-    $search = $_POST["search"];
-    if ($search != '') {
-        $query .= " WHERE name LIKE :search";
-        $params[":search"] = "%$search%";
+//Server Side Validation of Min and Max Price
+$isValid = true;
+if (isset($_POST["min-price"]) && isset($_POST["max-price"])) {
+    if ($_POST["min-price"] != "" && $_POST["max-price"] != "" && $_POST["min-price"] > $_POST["max-price"]) {
+        flash("Minimum price cannot be higher than maximum price", "warning");
+        $isValid = false;
     }
 }
-if (isset($_POST["category"])) {
-    $category = $_POST["category"];
-    if ($category != "all") {
-        if (!strpos($query, "WHERE")) {
-            $query .= " WHERE category=:category";
-        } else {
-            $query .= " AND category=:category";
-        }
-        $params[":category"] = $category;
-    }
-}
-if (isset($_POST["sort"])) {
-    $sort = $_POST["sort"];
-    if (!($sort == "none")) {
-        if ($sort == "low-high") {
-            $query .= " ORDER BY unit_price ASC";
-        } else {
-            $query .= " ORDER BY unit_price DESC";
+if ($isValid) {
+    $db = getDB();
+    $query = "SELECT name, unit_price FROM Products";
+    $params = [];
+    if (isset($_POST["search"])) {
+        $search = $_POST["search"];
+        if ($search != '') {
+            $query .= " WHERE name LIKE :search";
+            $params[":search"] = "%$search%";
         }
     }
+    if (isset($_POST["category"])) {
+        $category = $_POST["category"];
+        if ($category != "all") {
+            if (!strpos($query, "WHERE")) {
+                $query .= " WHERE category=:category";
+            } else {
+                $query .= " AND category=:category";
+            }
+            $params[":category"] = $category;
+        }
+    }
+    if (isset($_POST["min-price"])) {
+        $min = $_POST["min-price"];
+        if ($min != "") {
+            if (!strpos($query, "WHERE")) {
+                $query .= " WHERE unit_price >= :min";
+            } else {
+                $query .= " AND unit_price >= :min";
+            }
+            $params[":min"] = $min;
+        }
+    }
+    if (isset($_POST["max-price"])) {
+        $max = $_POST["max-price"];
+        if ($max != "") {
+            if (!strpos($query, "WHERE")) {
+                $query .= " WHERE unit_price <= :max";
+            } else {
+                $query .= " AND unit_price <= :max";
+            }
+            $params[":max"] = $max;
+        }
+    }
+    $query .= " ORDER BY modified DESC LIMIT 10";
+    $stmt = $db->prepare($query);
+    try {
+        $stmt->execute($params);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        flash("An unknown error occurred with loading the products", "warning");
+        error_log(var_export($e->errorInfo, true));
+    }
 }
-if (!strpos($query, "ORDER BY")) {
-    $query .= " ORDER BY modified DESC";
-} else {
-    $query .= " , modified DESC";
-}
-$query .= " LIMIT 10";
-$stmt = $db->prepare($query);
-try {
-    $stmt->execute($params);
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    flash("An unknown error occurred with loading the products", "warning");
-    error_log(var_export($e->errorInfo, true));
-}
+//TODO CACHE DATABASE RESULTS
 ?>
-<form class="row offset-lg-2" method="post" onsubmit="return validate(this)">
-    <div class="col-auto">
-        <label class="form-label" for="search">Search</label>
-        <input class="form-control" name="search" id="search" placeholder="Search" value="<?php se($search); ?>">
-    </div>
-    <div class="col-auto">
-        <label class="form-label" for="category">Category</label>
-        <select class="form-select" aria-label="Category" name="category" id="category">
-            <option value="all" <?php if (se($category, null, "", false) == "all") : ?>selected<?php endif; ?>>All</option>
-            <option value="sports" <?php if (se($category, null, "", false) == "sports") : ?>selected<?php endif; ?>>Sports</option>
-            <option value="electronics" <?php if (se($category, null, "", false) == "electronics") : ?>selected<?php endif; ?>>Electronics</option>
-            <option value="other" <?php if (se($category, null, "", false) == "other") : ?>selected<?php endif; ?>>Other</option>
-        </select>
-    </div>
-    <div class="col-auto">
-        <label class="form-label" for="sort">Price Sort</label>
-        <select class="form-select" aria-label="Sort" name="sort" id="sort">
-            <option value="none" <?php if (se($sort, null, "", false) == "none") : ?>selected<?php endif; ?>>None</option>
-            <option value="low-high" <?php if (se($sort, null, "", false) == "low-high") : ?>selected<?php endif; ?>>Low-High</option>
-            <option value="high-low" <?php if (se($sort, null, "", false) == "high-low") : ?>selected<?php endif; ?>>High-Low</option>
-        </select>
-    </div>
-    <div class="col-auto">
-        <div class="form-label invisible">Blank Text</div>
-        <input class="form-control btn btn-primary" type="submit" value="Search">
-    </div>
+<script>
+    //Client Side Validation
+    function validate(form) {
+        let isValid = true;
+        if (form["min-price"].value != "" && form["max-price"].value != "" && form["min-price"].value > form["max-price"].value) {
+            flash("Minimum price cannot be higher than maximum price", "warning");
+            isValid = false;
+        }
+        return isValid;
+    }
+</script>
+<form method="post" onsubmit="return validate(this)">
+    <label for="search">Search</label>
+    <input name="search" id="search" placeholder="Search" value="<?php se($search); ?>">
+    <label for="category">Category</label>
+    <select name="category" id="category">
+        <option value="all" <?php if (se($category, null, "", false) == "all") : ?>selected<?php endif; ?>>All</option>
+        <option value="sports" <?php if (se($category, null, "", false) == "sports") : ?>selected<?php endif; ?>>Sports</option>
+        <option value="electronics" <?php if (se($category, null, "", false) == "electronics") : ?>selected<?php endif; ?>>Electronics</option>
+        <option value="school" <?php if (se($category, null, "", false) == "school") : ?>selected<?php endif; ?>>School</option>
+        <option value="other" <?php if (se($category, null, "", false) == "other") : ?>selected<?php endif; ?>>Other</option>
+    </select>
+    <label for="min-price">Minimum Price</label>
+    <input type="number" step="0.01" name="min-price" id="min-price" value="<?php se($min); ?>">
+    <label for="max-price">Maximum Price</label>
+    <input type="number" step="0.01" name="max-price" id="max-price" value="<?php se($max); ?>">
+    <input type="submit" value="Search">
 </form>
-<div class="container-fluid">
-    <div class="row row-cols-sm-2 row-cols-xs-1 row-cols-md-3 row-cols-lg-6 g-4">
-        <?php foreach ($results as $result) : ?>
-            <div class="col">
-                <div class="card bg-light">
-                    <div class="card-body">
-                        <a class="text-dark text-decoration-none" href="more_details.php?name=<?php se($result, "name"); ?>">
-                            <h5 class="card-title"><?php se($result, "name"); ?></h5>
-                        </a>
-                        <?php if (se($result, "visibility", "", false) == "0") : ?>
-                            <p class="text-secondary">Not Visible</p>
-                        <?php else : ?>
-                            <p>Visible</p>
-                        <?php endif; ?>
-                        <p class="card-text">$<?php se($result, "unit_price"); ?></p>
-                        <a href="edit_item.php?name=<?php se($result, "name"); ?>">
-                            <div class="btn btn-secondary">Edit</div>
-                        </a>
-                    </div>
-                    <div class="card-footer">
-                        <form method="post" action="cart.php">
-                            <input type="hidden" name="name" value="<?php se($result, "name"); ?>" />
-                            <input type="submit" class="btn btn-primary" value="Add to Cart" />
-                        </form>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
-</div>
+<table class="table">
+    <thead>
+        <th>Name</th>
+        <th>Price</th>
+    </thead>
+    <tbody>
+        <?php if (empty($results)) : ?>
+            <tr>
+                <td colspan="100%">No products</td>
+            </tr>
+        <?php else : ?>
+            <?php foreach ($results as $result) : ?>
+                <tr>
+                    <td><a href="more_details.php?name=<?php se($result, "name"); ?>"><?php se($result, "name"); ?></a></td>
+                    <td>$<?php se($result, "unit_price"); ?></td>
+                    <?php if (is_logged_in()) : ?>
+                        <td><a href="cart.php?name=<?php se($result, "name"); ?>">Add To Cart</a></td>
+                    <?php endif; ?>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </tbody>
+</table>
+</table>
 <?php
 require(__DIR__ . "/../../partials/footer.php");
 ?>
